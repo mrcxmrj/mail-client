b7e296e159513c9bd21f30ce12195ce9eba26c52
mrcxmrj <marcin.zurawel@gmail.com>
2021-06-04 18:27:49 +0200

    added sending attachments

--- Sender.py
+++ Sender.py
@@ -10,7 +10,7 @@ class Sender:
         self.email = email
         self.password = password
 
-    def new_mail(self, to, subject, body_html):
+    def new_mail(self, to, subject, body_html, attachments=[]):
         self.msg = MIMEMultipart("alternative")
         self.msg["From"] = self.email
         self.msg["To"] = to
@@ -20,9 +20,19 @@ class Sender:
 
         text_part = MIMEText(body_text, "plain")
         html_part = MIMEText(body_html, "html")
-
+        
         self.msg.attach(text_part)
         self.msg.attach(html_part)
+
+        for file in attachments:
+            with open(file, "rb") as f:
+                    attach_part = MIMEBase("application", "octet-stream")
+                    attach_part.set_payload(f.read())
+                    
+            encoders.encode_base64(attach_part)
+        
+            attach_part.add_header("Content-Disposition", f"attachment; filename= {file}")
+            self.msg.attach(attach_part)
 
     def send_mail(self):
         if not hasattr(self, 'msg'):
@@ -43,5 +53,5 @@ class Sender:
 
 
 sender = Sender("mock60288@gmail.com", "mailclient123")
-sender.new_mail("xojicej620@revutap.com", "New message", "New message <b>test</b>")
+sender.new_mail("xojicej620@revutap.com", "New message", "New message <b>test</b>", ["attachments/text.txt", "attachments/image.jpg"])
 sender.send_mail()
